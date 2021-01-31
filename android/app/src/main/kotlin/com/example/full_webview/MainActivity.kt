be7c1480f4e25375cc39d6d79dfe959bf694484e
Samuel <samuelbeebest@gmail.com>
2021-01-31 18:32:29 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/full_webview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.full_webview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import 'webview/example1.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Webview Tutorial'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              MaterialButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => WebExampleOne()));
+                },
+                child: Text(
+                  'Example 1',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.yellow[900],
+                padding: EdgeInsets.symmetric(horizontal: 70, vertical: 12),
+              ),
+              SizedBox(height: 12),
+              MaterialButton(
+                onPressed: () {},
+                child: Text(
+                  'Example 2',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.green,
+                padding: EdgeInsets.symmetric(horizontal: 70, vertical: 12),
+              ),
+              SizedBox(height: 12),
+              MaterialButton(
+                onPressed: () {},
+                child: Text(
+                  'Example 3',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.purple[700],
+                padding: EdgeInsets.symmetric(horizontal: 70, vertical: 12),
+              ),
+              SizedBox(height: 12),
+              MaterialButton(
+                onPressed: () {},
+                child: Text(
+                  'Example 4',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.pink[900],
+                padding: EdgeInsets.symmetric(horizontal: 70, vertical: 12),
+              ),
+              SizedBox(height: 12),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
