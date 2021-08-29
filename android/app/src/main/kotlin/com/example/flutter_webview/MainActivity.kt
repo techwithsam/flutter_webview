556ea5ea785a3002f71540fe757e0bfd13bbfba8
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-30 00:48:26 +0100

    Bug fixed

--- android/app/src/main/kotlin/com/example/flutter_webview/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_webview
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/full_webview/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.techwithsam.flutter_webview
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
